1ca7fa00f0abe0fdeae784b865b45d0aab1a2135
gilzoide <gilzoide@gmail.com>
2020-06-11 10:32:07 -0300

    Add CLI script to transform a `nested` file into a `require`able lua file

---
+++ lua/nested2table.lua
@@ -0,0 +1,12 @@
+local nested = require 'nested'
+local inspect = require 'inspect'
+
+local arg = arg or {...}
+
+if arg[1] == '-' then arg[1] = false end
+if arg[2] == '-' then arg[2] = false end
+local input = arg[1] or io.stdin
+local contents = assert(nested.decode_file(input))
+local output = io.output(arg[2] or io.stdout)
+output:write('return ' .. inspect(contents))
+output:close()
