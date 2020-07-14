02748aa9b6f7890621c604425b02827430648343
gilzoide <gilzoide@gmail.com>
2020-07-14 14:46:13 -0300

    Remove nested.function as metatable, remove special 'function' keyword, add evaluate_with_env

--- examples/teste-function.lua
+++ examples/teste-function.lua
@@ -1,16 +1,38 @@
 nested = require 'nested'
 local nested_function = require 'nested.function'
+local nested_ordered = require 'nested.ordered'
 
-a = { 1, { segundo = 2 }, 3, b = 'b' }
+a = { 1, { segundo = 2 }, 3, b = 'B' }
 callable = setmetatable({}, {
     __call = function(self, ...)
         print('CALLABLE', ...)
     end
 })
+
+local _ENV = _ENV or getfenv()
+_ENV['function'] = function(arguments, body)
+    if not body then body, arguments = arguments, nil end
+    return function(...)
+        local env = _ENV or getfenv()
+        if arguments then
+            env = setmetatable({}, { __index = _ENV or getfenv() })
+            for i = 1, #arguments do
+                env[arguments[i]] = select(i, ...)
+            end
+        end
+        if type(body) == 'string' then
+            local chunk = assert(load(body, nil, 't', env))
+            return chunk(...)
+        else
+            return nested_function.evaluate_with_env(body, env)
+        end
+    end
+end
+
 local t = assert(nested.decode([=[
-f: [function [x y][
-    print OYES x y
-]]
+f: [function [x y] `
+    print(string.format('%d + %d = %d', x, y, x + y))
+`]
 
 f2: [function `print 'just print'`]
 
@@ -22,6 +44,6 @@ self.doidera.demais: 5
 [f2 ignored but evaluated args]
 [[function [print 'evaluate now']]]
 [callable tables work too]
-]=], nested.bool_number_filter, nested_function.new))
+]=], nested.bool_number_filter, nested_ordered.new))
 
-print(nested.encode(t()))
+print(nested.encode(nested_function.evaluate(t)))
--- lua/nested/function.lua
+++ lua/nested/function.lua
@@ -3,9 +3,7 @@ local nested_keypath = require 'nested.keypath'
 local unpack = unpack or table.unpack
 
 local nested_function = {}
-nested_function.__index = nested_function
 
-local ORDERED_KEY = '__nested_function_order'
 nested_function.escape_char = '\\'
 nested_function.keypath_pattern = '%.'
 
@@ -13,122 +11,60 @@ local function read_keypath(s)
     return nested_keypath.match(s, nested_function.keypath_pattern, nested_function.escape_char)
 end
 
-function nested_function.new()
-    return setmetatable({
-        [ORDERED_KEY] = {}
-    }, nested_function)
-end
-
-function nested_function.__newindex(t, index, value)
-    local order = rawget(t, ORDERED_KEY)
-    order[#order + 1] = index
-    rawset(t, index, value)
-end
-
-local function iterate_nested_function(t)
-    local i = 0
-    local keys = t[ORDERED_KEY]
-    return function()
-        i = i + 1
-        local idx = keys[i]
-        return idx, t[idx]
-    end
-end
-nested_function.__pairs = iterate_nested_function
-
-local function iterate_table(t)
-    if t[ORDERED_KEY] then
-        return iterate_nested_function(t)
+local function iscallable(v)
+    if type(v) == 'function' then
+        return true
     else
-        return pairs(t)
-    end
-end
-
-function nested_function.iscallable(v)
-    local vtype = type(v)
-    if vtype == 'table' or vtype == 'userdata' then
         local mt = getmetatable(v)
         return mt and mt.__call
     end
-    return vtype == 'function'
 end
 
-local loadstring_with_env
-if setfenv and loadstring then
-    loadstring_with_env = function(body, env)
-        local chunk, err = loadstring(body)
-        if not chunk then return nil, err end
-        return setfenv(chunk, env)
-    end
-else
-    loadstring_with_env = function(body, env)
-        return load(body, nil, 't', env)
-    end
-end
-
-local function evaluate_step(t, env)
+local function evaluate_step(t, read_env, ...)
     if type(t) == 'table' then
-        env = setmetatable({}, { __index = env })
-        if t[1] == 'function' then
-            local arguments, body = t[2], t[3]
-            if not body then body, arguments = arguments, nil end
-            return function(...)
-                if arguments then
-                    for i = 1, #arguments do
-                        env[arguments[i]] = select(i, ...)
-                    end
-                end
-                if type(body) == 'string' then
-                    local chunk, err = loadstring_with_env(body, env)
-                    if not chunk then return nil, err end
-                    return chunk()
-                else
-                    return evaluate_step(body, env)
-                end
-            end
-        else
-            local have_hash = false
-            for k, v in iterate_table(t) do
-                local key_not_numeric = type(k) ~= 'number'
-                have_hash = have_hash or key_not_numeric
-                v = evaluate_step(v, env)
-                if key_not_numeric then
-                    if nested.set_or_create(env, read_keypath(k), v) == nil then
-                        env[k] = v
-                    end
-                else
+        local env = setmetatable({}, { __index = read_env })
+        if select('#', ...) > 0 then env.arg = { ... } end
+        local have_hash = false
+        for k, v in pairs(t) do
+            local key_not_numeric = type(k) ~= 'number'
+            have_hash = have_hash or key_not_numeric
+            v = evaluate_step(v, env)
+            if key_not_numeric then
+                if nested.set(env, read_keypath(k), v) == nil then
                     env[k] = v
                 end
-            end
-            if nested_function.iscallable(env[1]) then
-                local f = table.remove(env, 1)
-                if have_hash then
-                    return f(env)
-                else
-                    return f(unpack(env))
-                end
             else
-                return env
+                env[k] = v
             end
+        end
+        if iscallable(env[1]) then
+            local f = table.remove(env, 1)
+            if have_hash then
+                return f(env)
+            else
+                return f(unpack(env))
+            end
+        else
+            return env
         end
     elseif type(t) == 'string' then
         local escape_length = #nested_function.escape_char
         if t:sub(1, escape_length) == nested_function.escape_char then
             return t:sub(escape_length + 1)
         else
-            return nested.get(env, read_keypath(t)) or t
+            return nested.get(read_env, read_keypath(t)) or t
         end
     else
         return t
     end
 end
 
-function nested_function.evaluate(t, fenv, ...)
-    local env = setmetatable({
-        arg = {...}
-    }, { __index = fenv or _ENV or getfenv() })
-    return evaluate_step(t, env)
+function nested_function.evaluate_with_env(t, fenv, ...)
+    return evaluate_step(t, read_env, ...)
 end
-nested_function.__call = nested_function.evaluate
+
+function nested_function.evaluate(t, ...)
+    return evaluate_step(t, _ENV or getfenv(), ...)
+end
 
 return nested_function
