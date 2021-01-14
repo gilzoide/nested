48d754e634ab65cffdd2ac0f0924988ffb1275e1
gilzoide <gilzoide@gmail.com>
2021-01-14 20:50:40 -0300

    Update stringstream version to 0.2.0, fix compact table representation, add scm-3 rockspec

--- lua/main.lua
+++ lua/main.lua
@@ -15,14 +15,14 @@ Options:
 if args.input_name == '-' then args.input:close(); args.input = io.stdin end
 if args.output_name == '-' then args.output:close(); args.output = io.stdout end
 
-local stream = stringstream.new(args.input, 4096)
+local stream = assert(stringstream.new(args.input, nil, 4096))
 local contents = assert(nested.decode(stream, {
     text_filter = args.table and nested.bool_number_filter or nil,
     table_constructor = function() return OrderedMap() end,
 }))
 
 if args.table then
-    local ret = args.tight and 'return' or 'return '
+    local ret = args.indent <= 0 and 'return' or 'return '
     args.output:write(ret .. pretty.write(contents, string.rep(' ', args.indent)))
 else
     assert(nested.encode_to_file(contents, args.output, args.indent))
