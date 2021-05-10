7575b8db696990d8d1d74e319f7ec933f3b861bf
gilzoide <gilzoide@gmail.com>
2021-05-09 22:16:16 -0300

    Add __tostring support when encoding tables

--- lua/nested.lua
+++ lua/nested.lua
@@ -281,6 +281,11 @@ local function not_quoted(token)
     return token:sub(1, 1) ~= '"' or token:sub(-1) ~= '"'
 end
 
+local function has_tostring(t)
+    local mt = getmetatable(t)
+    return mt and mt.__tostring
+end
+
 local function encode(t, indent_spaces)
     indent_spaces = math.min(math.floor(tonumber(indent_spaces) or 2), 8)
     local compact = indent_spaces < 0
@@ -303,7 +308,7 @@ local function encode(t, indent_spaces)
             if or_space then append(' ') end
         end
     local function encode_into(t, indent_level)
-        if type(t) == 'table' then
+        if type(t) == 'table' and not has_tostring(t) then
             if state[t] ~= nil then
                 -- t was already encoded, just add a reference to it
                 append(anchor_reference_mt.new(state[t]))
